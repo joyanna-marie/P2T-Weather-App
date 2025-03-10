e0768ee1294f9062867b0763c282734e0c0bb466
JoyAnna Hickey <jhickey09@gmail.com>
2025-03-10 19:11:22 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './index.css';
+import { getWeather } from './services/WeatherService';
+import { getDateFromHours } from './utils';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [city, setCity] = useState('New York');
+  const [weather, setWeather] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    getWeather(city)
+    .then((data) => {
+      setWeather(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError(error.message);
+      setLoading(false);
+    });
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Counter</h1>
-        <p>Current Count {count}</p>
-        <button type='button' onClick={() => setCount((prev) => prev + 1)}>Increment Count</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <main>
+    {error && <p>{error}</p>}
+    {loading ? (
+      <p>Weather Loading...</p>
+    ) : (
+      <section>
+        <form>
+          <label htmlFor="search-city">
+            <input type="text" name="search-city" id="search-city" placeholder='Search for Weather' />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+        <h1>Weather Details for: {city}</h1>
+        <p>{weather?.main.temp} | {weather?.weather[0].description}</p>
+        <p>{getDateFromHours(weather?.sys.sunset)}</p>
+        <p>{weather?.main.humidity}</p>
+        <p>{weather?.main.sea_level}</p>
+        <p>{weather?.wind.speed}</p>
+      </section>
+    )}
+    </main>
   );
 }
 
